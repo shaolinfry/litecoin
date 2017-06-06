62fdbf1d5c23e241a6422ab7889855bafb37910c
shaolinfry <shaolinfry@protonmail.ch>
2017-06-06 11:35:18 +0000

    Change period to 504 blocks at 65% for segsignal

--- src/versionbits.cpp
+++ src/versionbits.cpp
@@ -151,11 +151,20 @@ private:
 protected:
     int64_t BeginTime(const Consensus::Params& params) const { return params.vDeployments[id].nStartTime; }
     int64_t EndTime(const Consensus::Params& params) const { return params.vDeployments[id].nTimeout; }
-    int Period(const Consensus::Params& params) const { return params.nMinerConfirmationWindow; }
-    int Threshold(const Consensus::Params& params) const {
-        if (params.vDeployments[id].bit == params.vDeployments[Consensus::DEPLOYMENT_SEGSIGNAL].bit &&
+    int Period(const Consensus::Params& params) const {
+        if (params.nRuleChangeActivationThreshold == 1916 && params.nMinerConfirmationWindow == 2016 &&
+            params.vDeployments[id].bit == params.vDeployments[Consensus::DEPLOYMENT_SEGSIGNAL].bit &&
             params.vDeployments[id].nStartTime == params.vDeployments[Consensus::DEPLOYMENT_SEGSIGNAL].nStartTime) {
-            return 1311; // 65% threshold for SEGSIGNAL only
+            return 504; // 504 block period for SEGSIGNAL only
+        }
+        return params.nMinerConfirmationWindow;
+    }
+
+    int Threshold(const Consensus::Params& params) const {
+        if (params.nRuleChangeActivationThreshold == 1916 && params.nMinerConfirmationWindow == 2016 &&
+            params.vDeployments[id].bit == params.vDeployments[Consensus::DEPLOYMENT_SEGSIGNAL].bit &&
+            params.vDeployments[id].nStartTime == params.vDeployments[Consensus::DEPLOYMENT_SEGSIGNAL].nStartTime) {
+            return round(504 * 0.65); // 65% threshold for SEGSIGNAL only
         }
         return params.nRuleChangeActivationThreshold;
     }
